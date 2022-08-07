d73ae4b2716c4706342360662b148dcf9b9dcba3
Jinkyu Sung <sungjk1999@gmail.com>
2022-08-07 21:54:23 +0900

    codes backup

--- 06_Loops/main.go
+++ 06_Loops/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	menu := []string{"Hamburgers", "Cheeseburgers", "Fries"}
+
+	fmt.Println("The menu:")
+	for idx, food := range menu {
+		fmt.Println(idx, food)
+	}
+
+	orders := map[string]string{
+		"John":   "Cheeseburgers",
+		"Janet":  "Hamburgers",
+		"Jordan": "Fries",
+	}
+
+	orders["James"] = "Chicken Sandwich"
+
+	fmt.Println("\nThe friend's orders:")
+
+	for friend, order := range orders {
+		fmt.Println(friend, ":", order)
+	}
 }
--- 07_Arrays/main.go
+++ 07_Arrays/main.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+func changeLastElement(slice []int, element int) {
+	// Original slice value be changed
+	slice[len(slice)-1] = element
+}
+
+func appendLastElement(slice []int, element int) {
+	// Not work!
+	slice = append(slice, element)
+}
+
 func main() {
 
+	// Explict
+	var arr1 = [5]int{1, 2, 3, 4, 5} // or
+	var arr2 = [...]int{1, 2, 3, 4, 5}
+
+	// Implict
+	arr3 := [1]string{"Apple"}
+	fmt.Println(arr1, arr2, arr3)
+
+	// Explict
+	var slice1 = []int{1, 2, 3}
+
+	// Implict
+	slice2 := []string{"Samsung"}
+	fmt.Println(slice1, slice2)
+
+	changeLastElement(slice1, 5)
+	fmt.Println(slice1)
+
+	appendLastElement(slice1, 7) // Not work.
+	fmt.Println(slice1)
+
+	slice1 = append(slice1, 7)
+	fmt.Println(slice1)
 }
